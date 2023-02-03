a7ae727adabf58780af0777cf0c6eec241bef911
chen2021-web <83762621+chen2021-web@users.noreply.github.com>
2023-02-03 10:21:46 +0800

    还原错误更改

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2316,7 +2316,6 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 	static u32 ksu_sid;
 	char *secdata;
 	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
-	int nosuid = (bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID);
 	int rc,error;
 	u32 seclen;
 
